bda4adb3a61ea8a46b2aadf67be4b058ed451b70
Dao Tung <duytung95nb@gmail.com>
2018-12-02 17:03:32 +0700

    Authentication run on android

--- Backpacking/App.js
+++ Backpacking/App.js
@@ -5,45 +5,25 @@
  * @format
  * @flow
  */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+import { SwitchNavigator } from 'react-navigation';
+// import the different screens
+import Loading from './src/Loading';
+import SignUp from './src/SignUp';
+import Login from './src/Login';
+import Main from './src/Main';
+// create our app's navigation stack
+const App = SwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
   }
-}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
